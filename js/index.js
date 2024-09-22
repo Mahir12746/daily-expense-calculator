46f835ad5ee3f2c5a1da1a6479ecb609c5675ba1
Mahir Aseef <86533931+Mahir12746@users.noreply.github.com>
2024-09-23 00:51:44 +0600

    complete the tasks

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
-const income = parseFloat(document.getElementById("income").value);
-const software = parseFloat(document.getElementById("software").value);
-const courses = parseFloat(document.getElementById("courses").value);
-const internet = parseFloat(document.getElementById("internet").value);
+
 
 const calculateButton = document.getElementById("calculate");
 calculateButton.addEventListener('click', function(){
+
+    const income = parseFloat(document.getElementById("income").value);
+    const software = parseFloat(document.getElementById("software").value);
+    const courses = parseFloat(document.getElementById("courses").value);
+    const internet = parseFloat(document.getElementById("internet").value);
+
+    if (income <=0 || isNaN(income)) {
+        document.getElementById("income-error").classList.remove('hidden');
+        return;
+    }
 
     const totalExpenses = software + courses + internet;
     const balance = income - totalExpenses;
@@ -17,6 +24,20 @@ calculateButton.addEventListener('click', function(){
 
     const result = document.getElementById('results');
     result.classList.remove('hidden');
+
+    const historyItem = document.createElement("div");
+    historyItem.className = 'bg-white p-3 rounded-md border-1-2 border-indigo-500';
+
+    historyItem.innerHTML = `
+        <p class='text-xs text-gray-500'>${new Date().toLocaleDateString()}</p>
+        <p class='text-xs text-gray-500'>Income: ৳${income.toFixed(2)}</p>
+        <p class='text-xs text-gray-500'>Expenses: ৳${totalExpenses.toFixed(2)}</p>
+        <p class='text-xs text-gray-500'>Balance: ৳${balance.toFixed(2)}</p>
+
+    
+    `;
+    const historyContainer = document.getElementById('history-list');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild);
 });
 
 const calculateSavingsButton = document.getElementById('calculate-savings');
@@ -35,3 +56,23 @@ calculateSavingsButton.addEventListener('click', function() {
     const savingElement = document.getElementById('savings-amount');
     savingElement.innerText = savingAmount.toFixed(2);
 })
+
+const historyTab = document.getElementById('history-tab');
+const assistantTab = document.getElementById('assistant-tab');
+historyTab.addEventListener('click', function() {
+    historyTab.classList.add('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+    historyTab.classList.remove('text-gray-600');
+    assistantTab.classList.remove('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+    assistantTab.classList.add('text-gray-600');
+
+    document.getElementById('expense-form').classList.add('hidden');
+    document.getElementById('history-section').classList.remove('hidden');
+});
+
+assistantTab.addEventListener('click', function() {
+    assistantTab.classList.add('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+    historyTab.classList.remove('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+
+    document.getElementById('expense-form').classList.remove('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+});
